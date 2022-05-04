86be86ef66bcab48b1913b4fad54a8ddd1c73b2f
wanglun1996 <wanglun@berkeley.edu>
2022-05-04 15:30:32 -0700

    successfully aggregate commitment shares

--- ratel/genfiles/python/rockPaperScissors.py
+++ ratel/genfiles/python/rockPaperScissors.py
@@ -7,7 +7,7 @@ from ratel.src.python.utils import location_sharefile, prog, mpcPort, blsPrime, 
 # TODO: Manually added code for zkrp
 # TODO: this can only be run under the virtual environment. can we move the package to the whole environment?
 # from zkrp_pyo3 import zkrp_prove, zkrp_verify
-from ratel.src.zkrp_pyo3.zkrp_pyo3 import pedersen_commit, pedersen_open, zkrp_prove, zkrp_verify
+from ratel.src.zkrp_pyo3.zkrp_pyo3 import pedersen_aggregate, pedersen_commit, pedersen_open, zkrp_prove, zkrp_verify
 
 async def monitor(server, loop):
     blkNum = server.web3.eth.get_block_number()
@@ -46,8 +46,8 @@ async def runCreateGame(server, log):
     maskedValue1 = log['args']['maskedValue1']
 
     # TODO: 
-    proof, comm = zkrp_prove(2022, 32)
-    assert zkrp_verify(proof, comm), "[Error]: Committed secret value does not pass range proof verification!"
+    proof, commitment = zkrp_prove(2022, 32)
+    assert zkrp_verify(proof, commitment), "[Error]: Committed secret value does not pass range proof verification!"
 
     value1 = recover_input(server.db, maskedValue1, idxValue1)
     blinding = 10
@@ -56,17 +56,19 @@ async def runCreateGame(server, log):
     value1_bytes = list(value1.to_bytes(32, byteorder='little'))
     blinding_bytes = list(blinding.to_bytes(32, byteorder='little'))
 
-    commitment = pedersen_commit(value1_bytes, blinding_bytes)
+    share_commitment = pedersen_commit(value1_bytes, blinding_bytes)
     
     # TODO: create the function to commit to the unmasked secret shares.
     # TODO: we also need to change the current zkrp interface to allow specifying r and choose range to prove.
 
-    server.zkrpShares[f'{idxValue1}'].append(commitment)
-    print(f'^^^^^, {idxValue1}')
+    server.zkrpShares[f'{idxValue1}'].append(share_commitment)
     results = await server.get_zkrp_shares(players(server.contract), f'{idxValue1}')
     print('#####', results)
 
-    # assert comm_mpc_open(zkrp_shares) == comm
+    print("((((((Server players:", server.players)
+    agg_commitment = pedersen_aggregate(results, list(range(server.players)))
+
+    # assert agg_commitment == commitment
 
     await server.dbLock['access'].acquire()
     readKeys = []
